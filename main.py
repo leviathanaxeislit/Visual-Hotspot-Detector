6b732eab1efd0ec6127812cd56b9fe4099b961a1
Chandra Prakash <scp.thedevil@gmail.com>
2025-04-11 17:07:41 +0530

    Added a initial requirements file
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ async def detect_hotspots(request: HotspotRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
